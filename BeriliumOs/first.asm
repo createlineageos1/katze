47f2e7dbf6fa3a8ee528ed4d49390164169adfb8
developer-kenan <sagopakajmersf@gmail.com>
2003-01-01 03:13:49 +0200

    Beril seni çok seviyorum

--- BeriliumOs/first.asm
+++ BeriliumOs/first.asm
@@ -8,4 +8,51 @@ mov al, 'E'
 int 0x10 
 mov al, 'R'
 int 0x10
-mov al, 
+mov al, 'I'
+int 0x10
+mov al, 'L'
+int 0x10
+mov al, 'I'
+int 0x10
+mov al, 'U'
+int 0x10
+mov al, 'M'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'O'
+int 0x10
+mov al, 'S'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'A'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'H'
+int 0x10
+mov al, 'O'
+int 0x10
+mov al, 'S'
+int 0x10
+mov al, 'G'
+int 0x10
+mov al, 'E'
+int 0x10
+mov al, 'L'
+int 0x10
+mov al, 'D'
+int 0x10
+mov al, 'I'
+int 0x10
+mov al, 'N'
+int 0x10
+mov al, 'Î'
+int 0x10
+mov al, 'Z'
+int 0x10
+hlt
+jmp $
+times 510 -($-$$) db 0x0
+dw 0x55AA
